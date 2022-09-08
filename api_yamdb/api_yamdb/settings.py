96a367011c761101b4c99a6a4220b7c5c6010b0f
LazarevaKate <lazareva.ekaterina2@yandex.ru>
2022-09-08 22:38:16 +0300

    добавлены настройки

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -100,8 +100,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = [(os.path.join(BASE_DIR, 'static/')),
-                    ('redoc', '/app/templates/')]
+STATIC_ROOT = [(os.path.join(BASE_DIR, 'static/')), ('redoc', '/app/templates/')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
