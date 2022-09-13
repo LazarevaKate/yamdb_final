4a4c0f8379d00de4601c19f7b328f62b2e622933
LazarevaKate <lazareva.ekaterina2@yandex.ru>
2022-09-14 00:02:45 +0300

    добавлены настройки

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -101,9 +101,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'media/'),)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
